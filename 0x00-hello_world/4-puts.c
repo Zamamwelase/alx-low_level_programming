80948401929c7dec2990a4017d029f67f3369c6c
zamamwelase <zamamwelasemavundla@gmail.com>
2023-08-12 12:15:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,7 @@
  *  * File: 4-puts.c
  *   * Auth: Brennan D Baraban <375@holbertonschool.com>
  *    */
-
 #include <stdio.h>
-
 /**
  *  * main - Prints "Programming is like building a multilingual
  *   *                puzzle, followed by a new line.
@@ -14,6 +12,5 @@
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
 }
