c2af7bf746dddcfcb65e241f8e28db96f29302b2
zamamwelase <zamamwelasemavundla@gmail.com>
2023-08-14 15:14:04 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  *
  * * main - Entry point
@@ -8,7 +9,9 @@
  * * Return: Always 0 (Success)
  *
  * */
+
 int main(void)
+
 {
 
 	        printf("with proper grammar, but the outcome is a piece of art,\n");
