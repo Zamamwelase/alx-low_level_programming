5e03d32b7bd36e4d9883e02aa6aec2dc00bad192
zamamwelase <zamamwelasemavundla@gmail.com>
2023-08-15 15:09:32 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *  * main - main - print _putchar
- *   *
- *    * Return: Always 0.
- *     */
+ *  * main - print _putchar
+ *  * is written by zama
+ *  * Return: 0
+ *  */
 int main(void)
 {
-	_putchar('_');	
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
