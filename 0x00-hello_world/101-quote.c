4ab08e4022d10562a6dee49c8ea465096fa918ba
zamamwelase <zamamwelasemavundla@gmail.com>
2023-08-16 00:35:08 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
-*main - entry point
-*Return:1
-*/
+ * main - Entry point
+ * Return:1
+ */
 int main(void)
 {
-	fputs("and that piece of art is useful\"
-		       	- Dora Korpar,	2015-10-19\n", stdout);
+	Write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
-}	
+}
