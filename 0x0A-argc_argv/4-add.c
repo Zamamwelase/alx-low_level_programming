e1940e13eedf6875ef96b35a97eb52262e093ec2
zamamwelase <zamamwelasemavundla@gmail.com>
2023-09-04 17:27:22 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,23 +3,18 @@
 #include <string.h>
 #include <ctype.h>
 
-/**
- *  * main - adds positive numbers
- *   * @argc: n args
- *    * @argv: arr args
- *     * Return: 0
- */
 int main(int argc, char *argv[])
 {
 	unsigned int i, sum, num;
 
 	sum = 0;
 
-	if (argc < 3)
+	if (argc < 2)
 	{
-	printf("h%d\n", 0);
+	printf("%d\n", 0);
 	return (0);
 	}
+
 	while (argc-- && argc > 0)
 	{
 	for (i = 0; argv[argc][i] != '\0'; i++)
@@ -36,3 +31,4 @@ int main(int argc, char *argv[])
 	printf("%d\n", sum);
 	return (sum);
 }
+
